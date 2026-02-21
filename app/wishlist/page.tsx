cd3997bab5195efa8b4552ba4555b107209fa28e
Anggi Salsabila <247006111038@student.unsil.ac.id>
2026-02-21 14:36:55 +0700

    update project

--- app/wishlist/page.tsx
+++ app/wishlist/page.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 export default function WishlistPage() {
   const { wishlist, removeFromWishlist } = useWishlist();
 
+  console.log(wishlist);
   return (
     <main className="min-h-screen bg-[#f8f4ef] px-10 py-20">
       <h1 className="text-4xl text-center mb-10">
@@ -20,8 +21,8 @@ export default function WishlistPage() {
         <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
           {wishlist.map((item: any) => (
             <div
-              key={item.id}
-              className="bg-white rounded-2xl shadow-md p-5"
+                key={item.id}
+                className="bg-white rounded-2xl shadow-md p-5 flex flex-col items-center gap-4"
             >
               <button
                 onClick={() => removeFromWishlist(item.id)}
@@ -33,10 +34,10 @@ export default function WishlistPage() {
               <Image
                 src={item.thumbnail}
                 alt={item.title}
-                width={300}
-                height={300}
+                width={200}
+                height={200}
                 className="rounded-xl object-cover"
-              />
+                />
 
               <h2 className="mt-4 text-lg font-semibold">
                 {item.title}
